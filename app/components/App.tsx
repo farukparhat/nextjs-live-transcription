393edd855a12ec9e93c742bc511574f0e02894d4
farukparhat <faruk.parhat@outlook.com>
2025-06-07 12:18:04 -0700

    Remove header and footer

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -112,21 +112,12 @@ const App: () => JSX.Element = () => {
   }, [microphoneState, connectionState]);
 
   return (
-    <>
-      <div className="flex h-full antialiased">
-        <div className="flex flex-row h-full w-full overflow-x-hidden">
-          <div className="flex flex-col flex-auto h-full">
-            {/* height 100% minus 8rem */}
-            <div className="relative w-full h-full">
-              {microphone && <Visualizer microphone={microphone} />}
-              <div className="absolute bottom-[8rem]  inset-x-0 max-w-4xl mx-auto text-center">
-                {caption && <span className="bg-black/70 p-8">{caption}</span>}
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="relative w-full h-full">
+      {microphone && <Visualizer microphone={microphone} />}
+      <div className="absolute bottom-[8rem] inset-x-0 max-w-4xl mx-auto text-center">
+        {caption && <span className="bg-black/70 p-8">{caption}</span>}
       </div>
-    </>
+    </div>
   );
 };
 
